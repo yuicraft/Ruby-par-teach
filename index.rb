36249e5f99a997d76b2541be00ceb6ef7a2af315
yuicraft <twins20131988@gmail.com>
2018-09-25 23:53:23 +0900

    기본 설명

---
+++ index.rb
@@ -0,0 +1,22 @@
+#코딩 규격
+
+#클래스/모듈명
+#카멜 케이스(Camel Case)
+#대문자로 시작
+
+class SampleClass
+end
+
+module SampleModule
+end
+
+#매소드명
+#스네이크 케이스(Snake Case)
+
+def sample_method
+end
+
+#변수명
+#메소드랑 동일
+
+sample_variavle = "sample"
