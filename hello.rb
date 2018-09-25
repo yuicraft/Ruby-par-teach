b0569c2fc673150134ae3b3973c9d08581ce6d57
yuicraft <twins20131988@gmail.com>
2018-09-25 21:10:59 +0900

    주석 추가

--- hello.rb
+++ hello.rb
@@ -1,9 +1,12 @@
 def hello(names)
+#메소드 선언 = def 메소드명(인수)...end
     names.each do |name|
+    #전달받은 배열의 각 요소를 순서대로 출력    
         puts "HELLO #{name.upcase}"
+        #배열안의 요소를 대문자로 치환
     end
 end
 
 rubies = ['MRI', 'jruby', 'macruby']
-
+#변수명 = 식
 hello(rubies)
