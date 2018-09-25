080ed7538fd7b1755c5c2bffdc6ae3f1703d137f
yuicraft <twins20131988@gmail.com>
2018-09-25 21:52:34 +0900

    메소드 정의와 반환 값

---
+++ return.rb
@@ -0,0 +1,18 @@
+k = 1;
+def add(a,b)
+    a+b
+    #메소드 반환 값은 해당 메소드 안에서 마지막으로 계산되는 식의 값이다.
+end
+
+puts add(1,1)
+
+def add1(a,b)
+    return a+b
+    puts "이 문자열은 출력되지 않는다."
+    k = a*b
+    #return을 쓰면 그 값이 반환 값이 되고 그 후 구문은 실행되지 않는다. 
+
+end
+
+puts add1(1,2);
+puts k
